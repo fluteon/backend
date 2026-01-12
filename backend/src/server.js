dc8c89c161b418e5cc126039244ddcfc308953f8
fluteon <fluteoncompany@gmail.com>
2026-01-12 21:56:35 +0530

    Refactor server.js to set PORT directly

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,8 @@
 const { app } = require(".");
 const { connectDb } = require("./config/db");
 
-const PORT = process.env.PORT || 8000;
-app.listen(PORT, async () => {
-  await connectDb();
-  console.log("ecommerce api listing on port", PORT);
-});
+const PORT=8000;
+app.listen(PORT,async ()=>{
+    await connectDb()
+    console.log("ecommerce api listing on port ",PORT)
+})
