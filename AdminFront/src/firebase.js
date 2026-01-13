370586f2faf9e5591d723c9370855d9ab04b469f
fluteon <fluteoncompany@gmail.com>
2026-01-13 14:08:49 +0530

    Update Firebase configuration with new credentials

--- AdminFront/src/firebase.js
+++ AdminFront/src/firebase.js
@@ -19,12 +19,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBagglV-SuRFcZMLADPcKecNmd2NL0Zuro",
-  authDomain: "fluteon-35cf4.firebaseapp.com",
-  projectId: "fluteon-35cf4",
-  storageBucket: "fluteon-35cf4.appspot.com",
-  messagingSenderId: "your-id",
-  appId: "your-app-id",
+  apiKey: "AIzaSyAwN3iL5XVslbBD3CEe7aKxSvtwZmIMZT4",
+  authDomain: "fluteon-35cf4-9d993.firebaseapp.com",
+  projectId: "fluteon-35cf4-9d993",
+  storageBucket: "fluteon-35cf4-9d993.firebasestorage.app",
+  messagingSenderId: "587923428114",
+  appId: "1:587923428114:web:b9785903d69f06eea899cb",
+  measurementId: "G-DW8P5KNX7Z"
 };
 
 const app = initializeApp(firebaseConfig);
