84d87c8446831a03b6286d12efaf7e418e7476c5
fluteon <fluteon@users.noreply.github.com>
2026-01-21 13:50:15 +0530

    fix: remove unused state and handlers from AdminNavbar

--- AdminFront/src/Admin/Navigation/AdminNavbar.jsx
+++ AdminFront/src/Admin/Navigation/AdminNavbar.jsx
@@ -57,18 +57,8 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
 }));
 
 export default function AdminNavbar({handleSideBarViewInMobile}) {
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
   const theme = useTheme();
   const isLargeScreen = useMediaQuery(theme.breakpoints.up('lg'));
-
-  const handleProfileMenuOpen = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleMobileMenuOpen = (event) => {
-    setMobileMoreAnchorEl(event.currentTarget);
-  };
 
   const menuId = 'primary-search-account-menu';
   const mobileMenuId = 'primary-search-account-menu-mobile';
@@ -120,7 +110,6 @@ export default function AdminNavbar({handleSideBarViewInMobile}) {
               aria-label="account of current user"
               aria-controls={menuId}
               aria-haspopup="true"
-              onClick={handleProfileMenuOpen}
               color="inherit"
             >
               <AccountCircle />
@@ -132,7 +121,6 @@ export default function AdminNavbar({handleSideBarViewInMobile}) {
               aria-label="show more"
               aria-controls={mobileMenuId}
               aria-haspopup="true"
-              onClick={handleMobileMenuOpen}
               color="inherit"
             >
               <MoreIcon />
